e4299a1e33f570c5f7e288a7711577bdb7dfb871
atulverma1212 <verma.av1997@gmail.com>
2017-09-28 19:51:27 +0530

    Morris Preorder Traversal

---
+++ Morris_Preorder.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+
+
+node *inorder_predecessor(node *root)
+{
+    node *cur  = root->left;
+    while(cur->right && cur->right!=root)
+        cur=cur->right;
+    return cur;
+}
+
+void preorder_traversal(node *root)
+{
+    if(root==NULL)
+        return;
+    node *pred = NULL;    // pre order predecessor
+    node *current = root;
+    while(current)
+    {
+        if(current->left == NULL)
+        {
+            cout << current->data << " ";
+            current = current->right;
+        }
+        else
+        {
+            pred = inorder_predecessor(current);
+            if(pred->right == current)
+            {
+                pred->right=NULL;
+                current=current->right;
+            }
+            else
+            {
+                cout << current->data << " ";
+                pred->right=current;
+                current = current->left;
+            }
+        }
+    }
+}
+
+class Tree
+{
+    node *tree;
+public:
+    Tree() { tree = NULL; }
+    node *create(int d)
+    {
+        node *temp = new node;
+        temp->data = d;
+        temp->left = temp->right = NULL;
+        return temp;
+    }
+    void insert(int d)
+    {
+        node *newnode = create(d);
+        node *temp = tree;
+        node *parent = NULL;
+        if(tree==NULL)
+            tree=newnode;
+        else
+        {
+            while(temp!=NULL)
+            {
+                parent = temp;
+                if(d < temp->data)
+                    temp=temp->left;
+                else
+                    temp=temp->right;
+            }
+            if(d < parent->data)
+                parent->left = newnode;
+            else
+                parent->right = newnode;
+        }
+    }
+    void morris()
+    {
+        preorder_traversal(tree);
+        cout  <<  endl;
+    }
+};
+
+int main()
+{
+    Tree arr;
+    int str[] = {5,3,8,2,4,7,9};
+    int n = sizeof(str)/sizeof(str[0]);
+    for(int i=0;i<n;i++)
+        arr.insert(str[i]);
+    arr.morris();
+}
